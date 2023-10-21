ddaacaeae024b5f4438bff07d3c15214ce5eebfe
hhh7441 <hhh7441@naver.com>
2023-10-21 22:56:33 +0900

    자동배포 코드 주석 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,18 @@
+# 리포지토리 경로 설정
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-APP_NAME=demo
+# 애플리케이션 이름 설정
+APP_NAME=cahtez
+
+# WAR 파일 이름과 경로 설정
 WAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.war' | tail -n 1)
 WAR_PATH=$REPOSITORY/build/libs/$WAR_NAME
 
+# 현재 실행 중인 애플리케이션 프로세스 ID 가져오기
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
+# 현재 실행 중인 애플리케이션 종료
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -16,6 +22,7 @@ else
   sleep 5
 fi
 
+# 새 WAR 파일 배포 및 실행
 echo "> $WAR_PATH 배포"
 nohup java -jar \
         -Dspring.profiles.active=dev \
